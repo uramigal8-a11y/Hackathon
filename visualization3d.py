dd05c7e7161ad08e1d4470e9088d2f184bd52f28
Dan1k753 <danylo.hotsiiii@gmail.com>
2026-04-05 12:17:11 +0300

    змінено

--- visualization3d.py
+++ visualization3d.py
@@ -1,59 +1,67 @@
 import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.io as pio
 import plotly.graph_objects as go
 
-def create_3d_plot(file_path):
-    # Читання даних перенесено сюди, щоб не захаращувати app.py
-    df = pd.read_csv(file_path)
-    
-    fig = go.Figure(data=[go.Scatter3d(
-        x=df['x'],
-        y=df['y'],
-        z=df['z'],
-        mode='lines',
-        line=dict(
-            width=6,
-            color=df['speed'],
-            colorscale='Viridis',
-            showscale=True
-        )
-    )])
 
-    fig.update_layout(
-        paper_bgcolor='black',
-        font_color='#00FF00',
-        scene=dict(
-            bgcolor='black',
-            xaxis=dict(
-                gridcolor='#003300',
-                zerolinecolor='#00FF00',
-                backgroundcolor='black',
-                showbackground=True
-            ),
-            yaxis=dict(
-                gridcolor='#003300',
-                zerolinecolor='#00FF00',
-                backgroundcolor='black',
-                showbackground=True
-            ),
-            zaxis=dict(
-                gridcolor='#003300',
-                zerolinecolor='#00FF00',
-                backgroundcolor='black',
-                showbackground=True
-            ),
-            aspectmode='manual',
-            aspectratio=dict(x=1, y=1, z=0.5)
-        )
-    )
+pio.renderers.default = "colab"
 
-    fig.update_scenes(
-        xaxis_showgrid=True,
-        yaxis_showgrid=True,
-        zaxis_showgrid=True,
-        xaxis_zeroline=True,
-        yaxis_zeroline=True
+df = pd.read_csv('clean_data.csv')
+df.columns = df.columns.str.strip()
+fig = go.Figure(data=[go.Scatter3d(
+    x=df['x'],
+    y=df['y'],
+    z=df['z'],
+    mode='lines',
+    line=dict(
+        width=6,
+        color=df['speed'],
+        colorscale='YlOrRd',
+        showscale=True
     )
-    
-    fig.update_traces(line=dict(colorscale=['#003300', '#00FF00']))
-    
-    return fig
+)])
+fig.update_layout(
+
+    paper_bgcolor='white',
+    font_color='#1c63e8',
+
+    scene=dict(
+
+        bgcolor='white',
+
+
+        xaxis=dict(
+            gridcolor='white',
+            zerolinecolor='white',
+            backgroundcolor='#ecf0f1',
+            showbackground=True,
+            title='Схід'
+        ),
+        yaxis=dict(
+            gridcolor='white',
+            zerolinecolor='white',
+            backgroundcolor='#ecf0f1',
+            showbackground=True,
+            title='Північ'
+        ),
+        zaxis=dict(
+            gridcolor='white',
+            zerolinecolor='white',
+            backgroundcolor='#ecf0f1',
+            showbackground=True,
+            title='Висота'
+        ),
+        aspectmode='manual',
+        aspectratio=dict(x=1, y=1, z=0.5)
+    )
+)
+
+fig.update_scenes(
+    xaxis_showgrid=True,
+    yaxis_showgrid=True,
+    zaxis_showgrid=True,
+    xaxis_zeroline=True,
+    yaxis_zeroline=True
+)
+fig.show()
